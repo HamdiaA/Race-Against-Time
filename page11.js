21d0a2abc2e53f81229c75cb944b1255270cb404
HamdiaA <hamdiaaa258@gmail.com>
2020-12-07 11:38:06 -0500

    worked on responsiveness of pages

--- page11.js
+++ page11.js
@@ -1,12 +1,3 @@
-/*function swapPic() {
-    var closed = "images/blackclosedcase.PNG";
-    var opened = "images/vaccinesuitcase.jpg";
-
-    var imgElmnt = document.getElementById('swapPic');
-
-    imgElmnt.src = (imgElmnt.src === closed)? opened : closed;
-}*/
-
 function swapPic() {
     var img2 = "images/blackclosedcase.PNG";
     var img1 = "images/vaccinesuitcase.png";
